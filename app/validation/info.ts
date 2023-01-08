90138fe6272d45fac87b3843be5c3a661d5595e2
TheFallenSpirit <thefallenspirit@outlook.com>
2023-01-08 13:53:36 -0800

    Switch to correct param types

--- app/validation/info.ts
+++ app/validation/info.ts
@@ -4,6 +4,6 @@ import { FastifyReply, HookHandlerDoneFunction } from 'fastify';
 
 export default (req: InfoRequest, res: FastifyReply, next: HookHandlerDoneFunction) => {
     if (!req.params.id) return badRequest(res, 'ERR.PARAM.UNDEFINED', "The 'id' paramater is undefined.");
-    if (!req.query.episodes) return badRequest(res, 'ERR.PARAM.UNDEFINED', "The 'episodes' paramater is undefined.");
+    if (!req.query.episodes) return badRequest(res, 'ERR.QUERY.UNDEFINED', "The 'episodes' query paramater is undefined.");
     next();
 };
--- app/validation/search.ts
+++ app/validation/search.ts
@@ -3,6 +3,6 @@ import { SearchRequest } from '@/routes/search';
 import { FastifyReply, HookHandlerDoneFunction } from 'fastify';
 
 export default (req: SearchRequest, res: FastifyReply, next: HookHandlerDoneFunction) => {
-    if (!req.query.query) return badRequest(res, 'ERR.PARAM.UNDEFINED', "The 'query' paramater is undefined.");
+    if (!req.query.query) return badRequest(res, 'ERR.QUERY.UNDEFINED', "The 'query' query paramater is undefined.");
     next();
 };
