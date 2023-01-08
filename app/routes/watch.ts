b93defcbc091c6e7b7511eb7f65dcb44bd9528b2
TheFallenSpirit <thefallenspirit@outlook.com>
2023-01-08 01:32:38 -0800

    Adjust backend for quality selector

--- app/routes/watch.ts
+++ app/routes/watch.ts
@@ -7,22 +7,16 @@ interface Source {
     embedded: string;
 }
 
-interface Quality {
-    url: string;
-    quality: number;
-}
-
 export type WatchRequest = FastifyRequest<{ Params: { id: string } }>;
 const index = async (app: FastifyInstance, req: WatchRequest, res: FastifyReply) => {
     const id = req.params.id;
-    let source: Source|undefined = undefined;
-    let qualities: Quality[] = [];
     let def: string|undefined = undefined;
+    let source: Source|undefined = undefined;
     let backup: string|undefined = undefined;
     await axios.get(`https://api.consumet.org/anime/gogoanime/watch/${id}`).then(async (response) => {
         if (!response.data.sources) {
             await axios.get(`https://api.consumet.org/anime/gogoanime/servers/${id}`).then((sRes) => {
-                source = { url: '', embedded: sRes.data[4].url };
+                source = { url: '', embedded: sRes.data[0].url };
             });
         } else {
             const rawSources = response.data.sources;
@@ -36,29 +30,19 @@ const index = async (app: FastifyInstance, req: WatchRequest, res: FastifyReply)
                         case 'backup':
                             backup = src.url;
                             break;
-
-                        default:
-                            console.log(parseInt(src.quality.replace('p', '')));
-                            await qualities.push({ url: src.url, quality: parseInt(src.quality.replace('p', '')) });
-                            break;
                     }
                 }
             };
             console.log('d');
-            if (!qualities[0]) {
-                if (def) source = { url: def, embedded: response.data.headers.Referer }; else if (backup) source = { url: backup, embedded: response.data.headers.Referer };
-            } else {
-                const best = await qualities.map((q) => q.quality).sort((q1, q2) => q1 - q2)[qualities.length-1];
-                if (best !== 1080 && best !== 720) {
-                    if (def) source = { url: def, embedded: response.data.headers.Referer }; else if (backup) source = { url: backup, embedded: response.data.headers.Referer };
-                } else {
-                    source = { url: qualities.filter((q) => q.quality === best)[0].url, embedded: response.data.headers.Referer };
-                };
-            }
+            if (backup) {
+                source = { url: backup, embedded: response.data.headers.Referer };
+            } else if (def) {
+                source = { url: def, embedded: response.data.headers.Referer };
+            };
         }
-    }).catch(async (err) => {
+    }).catch(async () => {
         await axios.get(`https://api.consumet.org/anime/gogoanime/servers/${id}`).then((sRes) => {
-            source = { url: '', embedded: sRes.data[4].url };
+            source = { url: '', embedded: sRes.data[0].url };
         });
     });
     return success(res, source);
