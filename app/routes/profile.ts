c9d8d771c49c7914fcf30a00e9ace6f3d13d7e86
Vertixx <skemproof2@gmail.com>
2023-01-24 15:49:44 +0530

    changed uername to profile name

--- app/routes/profile.ts
+++ app/routes/profile.ts
@@ -6,7 +6,7 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 export type UserSearchRequest = FastifyRequest<{ Querystring: { username: string } }>;
 export default async (app: FastifyInstance, req: UserSearchRequest, res: FastifyReply) => {
     const username = req.query.username;
-    const user = await User.find({ username })
+    const user = await User.findOne({ profileName: username });
     if (!user) return userNotfound(res, "ERR.USER_NOT_FOUND", "The user you are looking for does not exist.")
     return success(res, user);
 };
