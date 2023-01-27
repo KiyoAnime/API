abff3a5bd234d578f725d4fb16d3a863e3ef6e52
TheFallenSpirit <thefallenspirit@outlook.com>
2023-01-26 18:27:42 -0800

    Add advanced search functionality

--- app/routes/search.ts
+++ app/routes/search.ts
@@ -13,15 +13,18 @@ interface Results {
     thumbnail: string;
 }
 
-export type SearchRequest = FastifyRequest<{ Querystring: { query: string } }>;
+export type SearchRequest = FastifyRequest<{ Querystring: { query?: string; genres?: string;  } }>;
 export default async (app: FastifyInstance, req: SearchRequest, res: FastifyReply) => {
-    const query = req.query.query;
-    const results: Results[] = [];
+    const params = new URLSearchParams();
+    if (req.query.query) params.append('query', req.query.query);
+    if (req.query.genres) params.append('genres', req.query.genres);
+    params.append('perPage', '105');
     // prettier-ignore
-    await axios.get(`https://apiconsumetorg-production.up.railway.app/meta/anilist/${query}`).then((response) => {
-        if (response.status !== 200) return serverError(res, 'ERR.REQUEST_FAILED', 'The request to the Consumet API failed. R=1'); // REASON 1
+    await axios.get(`https://apiconsumetorg-production.up.railway.app/meta/anilist/advanced-search?${params}`).then(async (response) => {
+        const results: Results[] = [];
         const animes = response.data.results;
-        for (const anime of animes) results.push({
+        console.log(animes);
+        for (const anime of animes) await results.push({
             id: parseInt(anime.id),
             title: anime.title.userPreferred,
             thumbnail: anime.image,
@@ -29,13 +32,13 @@ export default async (app: FastifyInstance, req: SearchRequest, res: FastifyRepl
             type: anime.type,
             released: anime.releaseDate
         });
-    }).catch(() => {
-        return serverError(res, 'ERR.REQUEST_FAILED', 'The request to the Consumet API failed. R=2'); //REASON 2
+        return success(res, results);
+    }).catch((err) => {
+        return serverError(res, 'ERR.REQUEST_FAILED', `The request to the Consumet API failed. R=1. Error: ${err.response.data}`); // REASON 1
     });
-    return success(res, results);
 };
 
 export const validation =  (req: SearchRequest, res: FastifyReply, next: HookHandlerDoneFunction) => {
-    if (!req.query.query) return badRequest(res, 'ERR.QUERY.UNDEFINED', "The 'query' query paramater is undefined.");
+    // if (!req.query.query) return badRequest(res, 'ERR.QUERY.UNDEFINED', "The 'query' query paramater is undefined.");
     next();
 };
